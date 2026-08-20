f1ecd24155f5f4ea53008fea29cbe972f40bfbfc
misschawarma-droid <misschawarma@gmail.com>
2026-08-20 19:06:43 +0200

    Fix

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ ALLOWED_ORIGINS = [
     "https://www.misschawarma.fr",
     "http://localhost:8080",
     "http://localhost:5173",
-  #  "http://192.168.0.12:8080",  # ton IP locale de dev 
     "http://192.168.1.29:8080",
-    "http://172.20.10.2:8080"
+    "http://172.20.10.2:8080",
+    "http://10.86.1.217:8080"
 ]
 
 app.add_middleware(
